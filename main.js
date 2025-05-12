df9216001dcba3e4f621fe61ffcce6d3a48bea2a
Adekola Muhammed <mohammedadekola306@gmail.com>
2025-05-12 12:30:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+//write a function that takes a string as a parameter and reversed string
+function  firstname(name){
+ console.log('hi; '+ name);
+}
+firstname('muhammed');
+ 
+function reverse(number) {
+    const reversed = number.toString().split('')
+
+}
+reverse('1234');
+// Write a function called lastLetter that takes a string as a parameter and returns the last character.
+
+function div (a,b) {
+    return (a / b)
+}
+console.log(div(6, 2))
